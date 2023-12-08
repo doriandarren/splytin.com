d13793688f2c6f3956b1e37fd56a644b6a56df7c
Dorian Gonzalez <doriandarren1@gmail.com>
2023-12-08 16:23:57 +0100

    enhance routes

--- src/components/template/Layout.vue
+++ src/components/template/Layout.vue
@@ -1,33 +1,44 @@
 <template>
+    
     <div class="flex justify-center rounded-lg  m-10 bg-slate-100">
         <!-- Menu -->
-        <ul class="flex space-x-6 mr-4 ">
+        <ul class="flex space-x-6 mr-4">
             <li class="mr-5">
                 <RouterLink :to="{ name: 'home' }" class="hover:text-blue-300">Home</RouterLink>
             </li>
             <li class="mr-5">
-                <RouterLink :to="{ name: 'resources-alerts' }" class="hover:text-blue-300">Alerts</RouterLink>
+                <RouterLink :to="{ name: 'tailwind-resources-component-alerts' }" class="hover:text-blue-300">Alerts</RouterLink>
             </li>
             <li class="mr-5">
-                <RouterLink :to="{ name: 'resources-forms' }" class="hover:text-blue-300">Forms</RouterLink>
+                <RouterLink :to="{ name: 'tailwind-resources-component-forms' }" class="hover:text-blue-300">Forms</RouterLink>
             </li>
             <li class="mr-5">
-                <RouterLink :to="{ name: 'resources-buttons' }" class="hover:text-blue-300">Buttons</RouterLink>
+                <RouterLink :to="{ name: 'tailwind-resources-component-buttons' }" class="hover:text-blue-300">Buttons</RouterLink>
             </li>
             <li class="mr-5">
-                <RouterLink :to="{ name: 'resources-badges' }" class="hover:text-blue-300">Badges</RouterLink>
+                <RouterLink :to="{ name: 'tailwind-resources-component-badges' }" class="hover:text-blue-300">Badges</RouterLink>
             </li>
             <li class="mr-5">
-                <RouterLink :to="{ name: 'resources-cards' }" class="hover:text-blue-300">Cards</RouterLink>
+                <RouterLink :to="{ name: 'tailwind-resources-component-cards' }" class="hover:text-blue-300">Cards</RouterLink>
             </li>
             <li class="mr-5">
-                <RouterLink :to="{ name: 'resources-avatars' }" class="hover:text-blue-300">Avatars</RouterLink>
+                <RouterLink :to="{ name: 'tailwind-resources-component-avatars' }" class="hover:text-blue-300">Avatars</RouterLink>
             </li>
             <li class="mr-5">
-                <RouterLink :to="{ name: 'resources-paginations' }" class="hover:text-blue-300">Paginations</RouterLink>
+                <RouterLink :to="{ name: 'tailwind-resources-component-paginations' }" class="hover:text-blue-300">Paginations</RouterLink>
             </li>
             <li class="mr-5">
-                <RouterLink :to="{ name: 'resources-breadcrumbs' }" class="hover:text-blue-300">Breadcrumbs</RouterLink>
+                <RouterLink :to="{ name: 'tailwind-resources-component-breadcrumbs' }" class="hover:text-blue-300">Breadcrumbs</RouterLink>
+            </li>
+        </ul>
+        <!-- End Menu -->
+    </div>
+
+    <div class="flex justify-center rounded-lg  m-10 bg-slate-100">
+        <!-- Menu -->
+        <ul class="flex space-x-6 mr-4">
+            <li class="mr-5">
+                <RouterLink :to="{ name: 'tailwind-resources-block-blog1' }" class="hover:text-blue-300">Blog1</RouterLink>
             </li>
         </ul>
         <!-- End Menu -->
--- src/router/index.js
+++ src/router/index.js
@@ -15,50 +15,50 @@ const router = createRouter({
       children: [
         //Routes components
         {
-          path: '/resources/alerts',
-          name: 'resources-alerts',
-          component: () => import('@/views/tailwind_resources/components/alerts/Alert.vue')
+          path: '/tailwind/resources/component/alerts',
+          name: 'tailwind-resources-component-alerts',
+          component: () => import('@/views/tailwind/resources/components/alerts/Alert.vue')
         },
         {
-          path: '/resources/forms',
-          name: 'resources-forms',
-          component: () => import('@/views/tailwind_resources/components/forms/Form.vue')
+          path: '/tailwind/resources/component/forms',
+          name: 'tailwind-resources-component-forms',
+          component: () => import('@/views/tailwind/resources/components/forms/Form.vue')
         },
         {
-          path: '/resources/buttons',
-          name: 'resources-buttons',
-          component: () => import('@/views/tailwind_resources/components/buttons/Button.vue')
+          path: '/tailwind/resources/component/buttons',
+          name: 'tailwind-resources-component-buttons',
+          component: () => import('@/views/tailwind/resources/components/buttons/Button.vue')
         },
         {
-          path: '/resources/badges',
-          name: 'resources-badges',
-          component: () => import('@/views/tailwind_resources/components/badges/Badge.vue')
+          path: '/tailwind/resources/component/badges',
+          name: 'tailwind-resources-component-badges',
+          component: () => import('@/views/tailwind/resources/components/badges/Badge.vue')
         },
         {
-          path: '/resources/cards',
-          name: 'resources-cards',
-          component: () => import('@/views/tailwind_resources/components/cards/Card.vue')
+          path: '/tailwind/resources/component/cards',
+          name: 'tailwind-resources-component-cards',
+          component: () => import('@/views/tailwind/resources/components/cards/Card.vue')
         },
         {
-          path: '/resources/avatars',
-          name: 'resources-avatars',
-          component: () => import('@/views/tailwind_resources/components/avatars/Avatar.vue')
+          path: '/tailwind/resources/component/avatars',
+          name: 'tailwind-resources-component-avatars',
+          component: () => import('@/views/tailwind/resources/components/avatars/Avatar.vue')
         },
         {
-          path: '/resources/paginations',
-          name: 'resources-paginations',
-          component: () => import('@/views/tailwind_resources/components/paginations/Pagination.vue')
+          path: '/tailwind/resources/component/paginations',
+          name: 'tailwind-resources-component-paginations',
+          component: () => import('@/views/tailwind/resources/components/paginations/Pagination.vue')
         },
         {
-          path: '/resources/breadcrumbs',
-          name: 'resources-breadcrumbs',
-          component: () => import('@/views/tailwind_resources/components/breadcrumbs/Breadcrumb.vue')
+          path: '/tailwind/resources/component/breadcrumbs',
+          name: 'tailwind-resources-component-breadcrumbs',
+          component: () => import('@/views/tailwind/resources/components/breadcrumbs/Breadcrumb.vue')
         },
         //Routes Block
         {
-          path: '/resources/block/blog1',
-          name: 'resources-block-blog1',
-          component: () => import('@/views/tailwind_resources/blog/Blog1.vue')
+          path: '/tailwind/resources/block/blog1',
+          name: 'tailwind-resources-block-blog1',
+          component: () => import('@/views/tailwind/resources/blocks/blog/Blog1.vue')
         },
       ]
     },
