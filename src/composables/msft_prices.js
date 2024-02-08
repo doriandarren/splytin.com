7c40334dd1ba2c1005c1d928d79c082c59d6e5f1
Dorian Gonzalez <doriandarren1@gmail.com>
2024-02-08 22:46:09 +0100

    add composable mile

--- src/composables/msft_prices.js
+++ src/composables/msft_prices.js
@@ -1,62 +1,157 @@
-import { ref } from "vue"
-
-
-
+import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
 
 
 export default function useMsftPrice() {
 
-    const msftPrice = ref();
-    const msftPrices = ref([]);
-    const msftPriceErrors = ref([]);
+	const msftPrice = ref();
+	const msftPrices = ref([]);
+	const msftPriceErrors = ref([]);
+	const { t } = useI18n();
 
 
 
-    const getMsftPrices = async () => {
-
-        await fetch('https://api.splytin.com/api/v1/b-msft-prices/list', {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        },
-    })
-        .then((res) => res.json())
-        .then((response) => {
-
-            // console.log(response.data);
-
-            msftPrices.value = response.data;
-
-        });
-    }
+    // const getMsftPricesMile = async () => {
+    //     await fetch('https://api.splytin.com/api/v1/b-msft-prices/list', {
+    //     method: "GET",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    // })
+    //     .then((res) => res.json())
+    //     .then((response) => {
+    //         // console.log(response.data);
+    //         msftPrices.value = response.data;
+    //     });
+    // }
 
 
-    const getMsftPrice = async (id) => {
+	const getMsftPrices = async () => {
+		msftPriceErrors.value = [];
+		await fetch(`https://api.splytin.com/api/v1/b-msft-prices/list`,{
+			method: 'GET',
+			headers: {
+				"Content-Type": "application/json",
+				//"Authorization": `Bearer ${localStorage.getItem('token')}`
+			},
+		})
+		.then(res => res.json())
+		.then((res) => {
+			if (!res.success) {
+				msftPriceErrors.value = res.errors;
+			}else{
+				msftPrices.value = res.data;
+			}
+		})
+		.catch((e) => {
+			msftPriceErrors.value.push(t("errors.error_internal"));
+		});
+	}
 
-        await fetch(`https://api.splytin.com/api/v1/b-msft-prices/show/${id}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        },
-    })
-        .then((res) => res.json())
-        .then((response) => {
 
-            // console.log(response.data);
+	const getMsftPrice = async (id) => {
+		msftPriceErrors.value = [];
+		await fetch(`https://api.splytin.com/api/v1/b-msft-prices/show/${id}`,{
+			method: 'GET',
+			headers: {
+				"Content-Type": "application/json",
+				//"Authorization": `Bearer ${localStorage.getItem('token')}`
+			},
+		})
+		.then(res => res.json())
+		.then((res) => {
+			if (!res.success) {
+				msftPriceErrors.value = res.errors;
+			}else{
+				msftPrice.value = res.data;
+			}
+		})
+		.catch((e) => {
+			msftPriceErrors.value.push(t("errors.error_internal"));
+		});
+	}
 
-            msftPrice.value = response.data;
 
-        });
-    }
-    
+	const storeMsftPrice = async (data) => {
+		msftPriceErrors.value = [];
+		await fetch(`https://api.splytin.com/api/v1/b-msft-prices/store`,{
+			method: 'POST',
+			headers: {
+				"Content-Type": "application/json",
+				//"Authorization": `Bearer ${localStorage.getItem('token')}`
+			},
+			body: JSON.stringify(data),
+		})
+		.then(res => res.json())
+		.then((res) => {
+			if (!res.success) {
+				msftPriceErrors.value = res.errors;
+			}else{
+				msftPrice.value = res.data;
+			}
+		})
+		.catch((e) => {
+			msftPriceErrors.value.push(t("errors.error_internal"));
+		});
+	}
 
-    return{
-        msftPrice, 
-        msftPrices,
-        msftPriceErrors,
-        getMsftPrices,
-        getMsftPrice
 
-    }
+	const updateMsftPrice = async (id, data) => {
+		msftPriceErrors.value = [];
+		await fetch(`https://api.splytin.com/api/v1/b-msft-prices/update/${id}`,{
+			method: 'PUT',
+			headers: {
+				"Content-Type": "application/json",
+				//"Authorization": `Bearer ${localStorage.getItem('token')}`
+			},
+			body: JSON.stringify(data),
+		})
+		.then(res => res.json())
+		.then((res) => {
+			if (!res.success) {
+				msftPriceErrors.value = res.errors;
+			}else{
+				msftPrice.value = res.data;
+			}
+		})
+		.catch((e) => {
+			msftPriceErrors.value.push(t("errors.error_internal"));
+		});
+	}
+
+
+	const destroyMsftPrice = async (id) => {
+		msftPriceErrors.value = [];
+		await fetch(`https://api.splytin.com/api/v1/b-msft-prices/delete/${id}`,{
+			method: 'DELETE',
+			headers: {
+				"Content-Type": "application/json",
+				//"Authorization": `Bearer ${localStorage.getItem('token')}`
+			},
+		})
+		.then(res => res.json())
+		.then((res) => {
+			if (!res.success) {
+				msftPriceErrors.value = res.errors;
+			}else{
+				msftPrice.value = res.data;
+			}
+		})
+		.catch((e) => {
+			msftPriceErrors.value.push(t("errors.error_internal"));
+		});
+	}
+
+
+	return {
+		msftPriceErrors,
+		msftPrice,
+		msftPrices,
+		getMsftPrice,
+		getMsftPrices,
+		storeMsftPrice,
+		updateMsftPrice,
+		destroyMsftPrice,
+	}
+
 }
-
