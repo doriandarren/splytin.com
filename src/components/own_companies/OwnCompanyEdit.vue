e40610d20ca98b7a53272783f7a6493341245881
Dilan <dilandarren1@gmail.com>
2024-02-21 20:30:42 +0100

    add own_company CEL = Create, Edit, List

--- src/components/own_companies/OwnCompanyEdit.vue
+++ src/components/own_companies/OwnCompanyEdit.vue
@@ -1,15 +1,152 @@
 <template>
-    <div>
 
-        <h1>Own company Edit</h1>
+	<!-- BEGIN: Card -->
+	<div class="card">
+		<!-- BEGIN: Form -->
+		<form class="validate-form" @submit.prevent="save">
 
-    </div>
+			<!-- BEGIN: container -->
+			<div class="grid grid-cols-12 gap-6">
+
+				<div class="col-span-12 md:col-span-6 lg:col-span-4">
+					<div class="input-form">
+						<label for="code" class="form-label w-full">
+							{{ $t("code") }} *
+						</label>
+						<input
+							v-model.trim="validate.code.$model"
+							id="code"
+							type="text"
+							name="code"
+							class="form-control"
+							:class="{ 'border-danger': validate.code.$error }"
+						/>
+						<template v-if="validate.code.$error">
+							<div v-for="(error, index) in validate.code.$errors" :key="index" class="text-danger mt-2">
+						{{ error.$message }}
+							</div>
+						</template>
+					</div>
+				</div>
+
+
+				<div class="col-span-12 md:col-span-6 lg:col-span-4">
+					<div class="input-form">
+						<label for="name" class="form-label w-full">
+							{{ $t("name") }} *
+						</label>
+						<input
+							v-model.trim="validate.name.$model"
+							id="name"
+							type="text"
+							name="name"
+							class="form-control"
+							:class="{ 'border-danger': validate.name.$error }"
+						/>
+						<template v-if="validate.name.$error">
+							<div v-for="(error, index) in validate.name.$errors" :key="index" class="text-danger mt-2">
+						{{ error.$message }}
+							</div>
+						</template>
+					</div>
+				</div>
+
+
+				<div class="col-span-12 md:col-span-6 lg:col-span-4">
+					<div class="input-form">
+						<label for="address" class="form-label w-full">
+							{{ $t("address") }} *
+						</label>
+						<input
+							v-model.trim="validate.address.$model"
+							id="address"
+							type="text"
+							name="address"
+							class="form-control"
+							:class="{ 'border-danger': validate.address.$error }"
+						/>
+						<template v-if="validate.address.$error">
+							<div v-for="(error, index) in validate.address.$errors" :key="index" class="text-danger mt-2">
+						{{ error.$message }}
+							</div>
+						</template>
+					</div>
+				</div>
+
+
+				<!-- BEGIN: Buttons -->
+				<div class="col-span-12 md:col-span-12 lg:col-span-12">
+					<div class="flex justify-center">
+						<button type="submit" class="btn btn-primary mr-5">
+							{{ $t("save") }}
+						</button>
+						<button @click.prevent="emit('cancelEdit')" class="btn btn-danger">
+							{{ $t("cancel") }}
+						</button>
+					</div>
+				</div>
+				<!-- END: Buttons -->
+
+			</div>
+			<!-- END: container -->
+
+		</form>
+		<!-- END: Form -->
+
+	</div>
+	<!-- END: Card -->
+
 </template>
+
 
 <script setup>
 
+	import { onMounted, reactive, toRefs } from 'vue';
+	import useOwnCompanies from '@/composables/own_companies';
+	import { required, minLength, maxLength, email, url, integer } from '@vuelidate/validators';
+	import { useVuelidate } from '@vuelidate/core';
+	import { helpers } from '@vuelidate/validators';
+	import { useI18n } from 'vue-i18n';
+
+	const { ownCompany, getOwnCompany } = useOwnCompanies();
+	const { t } = useI18n();
+	const props = defineProps(['ownCompanyId']);
+	const emit = defineEmits(['cancelEdit', 'updateOwnCompanyForm']);
+
+	const rules = {
+		code: {
+			required: helpers.withMessage(t("form.required"), required),
+		},
+		name: {
+			required: helpers.withMessage(t("form.required"), required),
+		},
+		address: {
+			required: helpers.withMessage(t("form.required"), required),
+		},
+	};
+
+	const formData = reactive({
+		code: "",
+		name: "",
+		address: "",
+	});
+
+	const validate = useVuelidate(rules, toRefs(formData));
+
+	const save = () => {
+		validate.value.$touch();
+		if (validate.value.$invalid) {
+			//TODO
+		} else {
+			emit('updateOwnCompanyForm', ownCompany.value.id, formData);
+		}
+	};
+
+	onMounted(async () => {
+		await getOwnCompany(props.ownCompanyId);
+		formData.code = ownCompany.value.code;
+		formData.name = ownCompany.value.name;
+		formData.address = ownCompany.value.address;
+	});
+
 </script>
-
-<style scoped>
-
-</style>
