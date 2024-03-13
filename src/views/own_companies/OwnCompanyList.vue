765f8d83d5302afe560bdf593cf0d5feeb2b2cca
Dorian Gonzalez <doriandarren1@gmail.com>
2024-03-13 19:49:48 +0100

    own_companies

--- src/views/own_companies/OwnCompanyList.vue
+++ src/views/own_companies/OwnCompanyList.vue
@@ -1,62 +1,77 @@
 <template>
 
-	<div v-animate>
+	<!-- BEGIN: Page Layout Create -->
+	<div v-animate v-if="isCreate">
+		<Create
+			@saveOwnCompanyForm="saveOwnCompanyForm"
+			@cancelCreate="cancelCreate"
+		/>
+	</div>
 
-		<!-- BEGIN: Page Layout Create -->
-		<div v-animate v-if="isCreate">
-			<Create
-				@saveOwnCompanyForm="saveOwnCompanyForm"
-				@cancelCreate="cancelCreate"
-			/>
-		</div>
+	<!-- BEGIN: Page Layout Update -->
+	<div v-animate v-if="isEdit">
+		<Edit
+			:ownCompanyId="ownCompanyId"
+			@cancelEdit="cancelEdit"
+			@updateOwnCompanyForm="updateOwnCompanyForm"
+		/>
+	</div>
 
-		<!-- BEGIN: Page Layout Update -->
-		<div v-animate v-if="isEdit">
-			<Edit
-				:ownCompanyId="ownCompanyId"
-				@cancelEdit="cancelEdit"
-				@updateOwnCompanyForm="updateOwnCompanyForm"
-			/>
-		</div>
-
-		<div v-animate class="p-5 border rounded-md shadow-sm" id="div_table">
-			<div class="flex flex-col sm:flex-row sm:items-end xl:items-start">
-				<form id="tabulator-html-filter-form" class="xl:flex sm:mr-auto">
-					<div class="relative sm:flex items-center sm:mr-4 mt-2 xl:mt-0">
-						<input
-							id="tabulator-html-filter-value"
-							v-model="filter.value"
-							type="text"
-							class="w-full xl:w-[600px] form-control mt-2 sm:mt-0"
-							:placeholder="$t('search')"
-							@keyup="onFilter" />
-							<button
-								class="w-4 h-4 absolute my-auto inset-y-0 mr-3 right-0 text-slate-400 hover:cursor-pointer"
-								@click.prevent="onResetFilter"
-							>
-							<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6c0 1.01-.25 1.97-.7 2.8l1.46 1.46A7.93 7.93 0 0 0 20 12c0-4.42-3.58-8-8-8m0 14c-3.31 0-6-2.69-6-6c0-1.01.25-1.97.7-2.8L5.24 7.74A7.93 7.93 0 0 0 4 12c0 4.42 3.58 8 8 8v3l4-4l-4-4z"/></svg>
-						</button>
-					</div>
-				</form>
-				<div class="flex mt-5 sm:mt-0">
-					<button class="btn-primary w-1/2 sm:w-auto mr-2" @click.prevent="showCreateOwnCompany">
-						<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 50 50"><path fill="currentColor" d="M25 42c-9.4 0-17-7.6-17-17S15.6 8 25 8s17 7.6 17 17s-7.6 17-17 17m0-32c-8.3 0-15 6.7-15 15s6.7 15 15 15s15-6.7 15-15s-6.7-15-15-15"/><path fill="currentColor" d="M16 24h18v2H16z"/><path fill="currentColor" d="M24 16h2v18h-2z"/></svg>
-					</button>
-				</div>
-			</div>
-			<div class="overflow-x-auto scrollbar-hidden">
-				<div
-					id="tabulator"
-					ref="table"
-					class="mt-5 table-report table-report--tabulator"
-				></div>
-			</div>
+	<!-- BEGIN: Table -->
+	<div v-animate class="p-5 border rounded-md shadow-sm" id="div_table">
+		<div class="flex flex-col sm:flex-row sm:items-end xl:items-start justify-end">
+			<div class="flex mt-5 mb-5 sm:mt-0">
+				<button class="btn-primary w-1/2 sm:w-auto" @click.prevent="showCreateCompany">
+				<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 50 50">
+				<path fill="currentColor"
+					d="M25 42c-9.4 0-17-7.6-17-17S15.6 8 25 8s17 7.6 17 17s-7.6 17-17 17m0-32c-8.3 0-15 6.7-15 15s6.7 15 15 15s15-6.7 15-15s-6.7-15-15-15" />
+					<path fill="currentColor" d="M16 24h18v2H16z" />
+					<path fill="currentColor" d="M24 16h2v18h-2z" />
+				</svg>
+			</button>
 		</div>
 	</div>
+
+	<!-- BEGIN: Table -->
+	<div class="overflow-x-auto scrollbar-hidden">
+		<VueGoodTable
+			:columns="columns" 
+			:rows="rows" 
+			:pagination-options="{
+				enabled: true,
+				mode: 'records',
+				perPage: 5,
+				position: 'bottton',
+				perPageDropdown: [10, 20, 50],
+				dropdownAllowAll: false,
+				setCurrentPage: 1,
+				nextLabel: 'Sig',
+				prevLabel: 'Prev',
+				rowsPerPageLabel: 'Rows per page',
+				ofLabel: 'de',
+				pageLabel: 'page', // for pages mode
+				allLabel: 'All',
+			}" 
+			:search-options="{ enabled: true, placeholder: 'Buscar' }"
+		>
+			<template #table-row="props">
+				<span v-if="props.column.field == 'actions'">
+					<button @click="showEditCompany(props.row.id)">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-600 hover:text-blue-400" viewBox="0 0 24 24"><path fill="currentColor" d="m7 17.013l4.413-.015l9.632-9.54c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.756-.756-2.075-.752-2.825-.003L7 12.583zM18.045 4.458l1.589 1.583l-1.597 1.582l-1.586-1.585zM9 13.417l6.03-5.973l1.586 1.586l-6.029 5.971L9 15.006z"/><path fill="currentColor" d="M5 21h14c1.103 0 2-.897 2-2v-8.668l-2 2V19H8.158c-.026 0-.053.01-.079.01c-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2"/></svg>
+					</button>
+					<button @click="showDeleteCompany(props.row.id)">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-600 hover:text-red-400" viewBox="0 0 24 24"><path fill="currentColor" d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2zM7 6v13z"/></svg>
+					</button>
+					</span>
+				</template>
+			</VueGoodTable>
+		</div>
+
+	</div>
+
 </template>
 <script setup>
-	import { ref, reactive, onMounted, toRaw } from 'vue';
-	import { TabulatorFull as Tabulator } from 'tabulator-tables';
+	import { ref, onMounted, toRaw } from 'vue';
 	import { useI18n } from 'vue-i18n';
 	import { Toast } from '@/utils/toast';
 	import Swal from 'sweetalert2';
@@ -65,9 +80,7 @@
 	import Edit from "@/components/own_companies/OwnCompanyEdit.vue";
 
 	// Tabulator
-	const table = ref(null);
-	const tabulator = ref(null);
-	const tableData = reactive([]);
+	const rows = ref([]);
 
 	// Views
 	const isCreate = ref(false);
@@ -78,186 +91,25 @@
 	const { ownCompanies, getOwnCompanies, storeOwnCompany, updateOwnCompany, destroyOwnCompany} = useOwnCompanies();
 
 
-	const filter = reactive({
-		field: "country_id",
-		type: "like",
-		value: "",
-	});
-
-	// Filter function
-	const onFilter = () => {
-		//tabulator.value.setFilter(filter.field, filter.type, filter.value);
-		tabulator.value.setFilter([
-			[
-				{field: 'country_id', type: 'like', value: filter.value},
-				{field: 'name', type: 'like', value: filter.value},
-				{field: 'tax', type: 'like', value: filter.value},
-				{field: 'address', type: 'like', value: filter.value},
-				{field: 'state', type: 'like', value: filter.value},
-				{field: 'municipality', type: 'like', value: filter.value},
-				{field: 'email', type: 'like', value: filter.value},
-				{field: 'phone', type: 'like', value: filter.value},
-				{field: 'zip_code', type: 'like', value: filter.value},
-				{field: 'website', type: 'like', value: filter.value},
-			]
-		]);
-	};
-
-	// On reset filter
-	const onResetFilter = () => {
-		filter.field = "country_id";
-		filter.type = "like";
-		filter.value = "";
-		onFilter();
-	};
-
 	const findData = async() => {
 		await getOwnCompanies();
 		return toRaw(ownCompanies.value);
 	}
 
 	// Table
-	const initTabulator = async () => {
-		tabulator.value = new Tabulator(table.value, {
-			pagination: "local",
-			paginationSize: 10,
-			paginationSizeSelector: [10, 20, 30, 40],
-			layout:"fitDataFill",
-			responsiveLayout: "collapse",
-			data: tableData.value, //link data to table
-			reactiveData: true, //enable data reactivity
-			columns: [
-				{
-					formatter: "responsiveCollapse",
-					width: 40,
-					minWidth: 30,
-					hozAlign: "center",
-					resizable: false,
-					headerSort: false,
-				},
-				{
-					title: t("country_id"),
-					minWidth: 200,
-					responsive: 0,
-					field: "country_id",
-					vertAlign: "middle",
-					headerHozAlign:"left",
-				},
-				{
-					title: t("name"),
-					minWidth: 200,
-					responsive: 0,
-					field: "name",
-					vertAlign: "middle",
-					headerHozAlign:"left",
-				},
-				{
-					title: t("tax"),
-					minWidth: 200,
-					responsive: 0,
-					field: "tax",
-					vertAlign: "middle",
-					headerHozAlign:"left",
-				},
-				{
-					title: t("address"),
-					minWidth: 200,
-					responsive: 0,
-					field: "address",
-					vertAlign: "middle",
-					headerHozAlign:"left",
-				},
-				{
-					title: t("state"),
-					minWidth: 200,
-					responsive: 0,
-					field: "state",
-					vertAlign: "middle",
-					headerHozAlign:"left",
-				},
-				{
-					title: t("municipality"),
-					minWidth: 200,
-					responsive: 0,
-					field: "municipality",
-					vertAlign: "middle",
-					headerHozAlign:"left",
-				},
-				{
-					title: t("email"),
-					minWidth: 200,
-					responsive: 0,
-					field: "email",
-					vertAlign: "middle",
-					headerHozAlign:"left",
-				},
-				{
-					title: t("phone"),
-					minWidth: 200,
-					responsive: 0,
-					field: "phone",
-					vertAlign: "middle",
-					headerHozAlign:"left",
-				},
-				{
-					title: t("zip_code"),
-					minWidth: 200,
-					responsive: 0,
-					field: "zip_code",
-					vertAlign: "middle",
-					headerHozAlign:"left",
-				},
-				{
-					title: t("website"),
-					minWidth: 200,
-					responsive: 0,
-					field: "website",
-					vertAlign: "middle",
-					headerHozAlign:"left",
-				},
-				{
-					title: "",
-					minWidth: 80,
-					width: 10,
-					field: "actions",
-					responsive: 0,
-					headerHozAlign: "center",
-					hozAlign: "center",
-					vertAlign: "middle",
-					headerSort: false,
-					download: false,
-					formatter: (cell, formatterParams) => {
-						return `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-600 hover:text-blue-400" viewBox="0 0 24 24"><path fill="currentColor" d="m7 17.013l4.413-.015l9.632-9.54c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.756-.756-2.075-.752-2.825-.003L7 12.583zM18.045 4.458l1.589 1.583l-1.597 1.582l-1.586-1.585zM9 13.417l6.03-5.973l1.586 1.586l-6.029 5.971L9 15.006z"/><path fill="currentColor" d="M5 21h14c1.103 0 2-.897 2-2v-8.668l-2 2V19H8.158c-.026 0-.053.01-.079.01c-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2"/></svg>`;
-					},
-					cellClick: (e, cell) => {
-						showEditOwnCompany(cell.getData().id);
-						e.preventDefault();
-					},
-				},
-				{
-					title: "",
-					minWidth: 80,
-					width: 10,
-					field: "actions",
-					responsive: 0,
-					headerHozAlign: "center",
-					hozAlign: "center",
-					vertAlign: "middle",
-					headerSort: false,
-					download: false,
-					formatter: (cell, formatterParams) => {
-						return `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-600 hover:text-red-400" viewBox="0 0 24 24"><path fill="currentColor" d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2zM7 6v13z"/></svg>`;
-					},
-					cellClick: (e, cell) => {
-						//deleteOwnCompany(cell.getData().id, cell.getData().name);
-						showDeleteOwnCompany(cell.getData().id);
-						e.preventDefault();
-					},
-				},
-			],
-		});
-	}
-
+	const columns = [
+		{ title: t("country_id"), field: 'country_id' },
+		{ title: t("name"), field: 'name' },
+		{ title: t("tax"), field: 'tax' },
+		{ title: t("address"), field: 'address' },
+		{ title: t("state"), field: 'state' },
+		{ title: t("municipality"), field: 'municipality' },
+		{ title: t("email"), field: 'email' },
+		{ title: t("phone"), field: 'phone' },
+		{ title: t("zip_code"), field: 'zip_code' },
+		{ title: t("website"), field: 'website' },
+		{ label: t('Actions'), field: 'actions', sortable: false, searchable: false, width: '100px',},
+	];
 	//Store
 	const showCreateOwnCompany = () => {
 		isCreate.value = true;
@@ -273,9 +125,7 @@
 		isCreate.value = false;
 		div_table.style.display = 'block';
 		await storeOwnCompany({ ...form });
-		//await getOwnCompanies();
-		tableData.value = await findData();
-		tabulator.value.setData(tableData.value);
+		rows.value = await findData();
 		await Toast(t("message.record_saved"), 'success');
 	}
 
@@ -295,9 +145,7 @@
 		isEdit.value = false;
 		div_table.style.display = 'block';
 		await updateOwnCompany(id, data);
-		//await getOwnCompanies();
-		tableData.value = await findData();
-		tabulator.value.setData(tableData.value);
+		rows.value = await findData();
 		await Toast(t("message.record_updated"), 'success');
 	}
 
@@ -313,7 +161,7 @@
 		}).then(async(result) => {
 			if (result.isConfirmed) {
 				await destroyOwnCompany(id);
-				await getOwnCompanies();
+		rows.value = await findData();
 				Swal.fire(t("message.record_deleted"), '', 'success');
 			}
 
@@ -322,8 +170,7 @@
 	}
 
 	onMounted(async () => {
-		tableData.value = await findData();
-		await initTabulator();
+		rows.value = await findData();
 	});
 
 
