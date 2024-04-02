c567a54dba72be1757286f46d4ab4995c8ea1bc9
Dorian Gonzalez <doriandarren1@gmail.com>
2024-04-02 23:23:06 +0200

    fix dashboard

--- src/views/dashboard/DashboardList.vue
+++ src/views/dashboard/DashboardList.vue
@@ -1,78 +1,85 @@
 <template>
-    
-     <!-- BEGIN: Page Layout -->
-  <div class="relative">
-    <div class="grid grid-cols-12 gap-6">
-      <div class="col-span-12 mt-8">
-        <div class="intro-y flex items-center h-10">
-          <h2 class="text-lg font-medium truncate mr-5">{{ $t("dashboard.title") }} 
-            <span class="text-xl font-bold">-
-            </span>
-          </h2>
-          <a href="" class="ml-auto flex items-center text-primary dark:text-light">
-          </a>
+
+    <div v-animate>
+
+        <!-- BEGIN: Page Layout -->
+        <div class="relative">
+            <div class="grid grid-cols-12 gap-6">
+                <div class="col-span-12 mt-8">
+                    <div class="intro-y flex items-center h-10">
+                        <h2 class="text-lg font-medium truncate mr-5">{{ $t("dashboard.title") }}
+                            <span class="text-xl font-bold">-
+                            </span>
+                        </h2>
+                        <a href="" class="ml-auto flex items-center text-primary dark:text-light">
+                        </a>
+                    </div>
+                    <div class="grid grid-cols-12 gap-6 mt-5">
+                        <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                            <div class="report-box zoom-in">
+                                <div class="box p-5">
+                                    <div class="flex align-middle">
+                                        <div class="text-3xl font-medium leading-8 text-gray-500">-</div>
+                                        <div class="ml-auto">
+                                            <NavigationIcon class="report-box__icon text-gray-500" />
+                                        </div>
+                                    </div>
+                                    <div class="text-base text-slate-500 mt-3">{{ $t("trip_created") }}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                            <div class="report-box zoom-in">
+                                <div class="box p-5">
+                                    <div class="flex align-middle">
+                                        <div class="text-3xl font-medium leading-8 text-orange-300">-</div>
+                                        <div class="ml-auto">
+                                            <AlertCircleIcon class="report-box__icon text-orange-300" />
+                                        </div>
+                                    </div>
+                                    <div class="text-base text-slate-500 mt-3">{{ $t("trip_pending") }}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                            <div class="report-box zoom-in">
+                                <div class="box p-5">
+                                    <div class="flex align-middle">
+                                        <div class="text-3xl font-medium leading-8 text-blue-300">-</div>
+                                        <div class="ml-auto">
+                                            <ActivityIcon class="report-box__icon text-blue-300" />
+                                        </div>
+                                    </div>
+                                    <div class="text-base text-slate-500 mt-3">{{ $t("trip_progress") }}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                            <div class="report-box zoom-in">
+                                <div class="box p-5">
+                                    <div class="flex align-middle">
+                                        <div class="text-3xl font-medium leading-8 text-green-300">-</div>
+                                        <div class="ml-auto">
+                                            <CheckIcon class="report-box__icon text-green-300" />
+                                        </div>
+                                    </div>
+                                    <div class="text-base text-slate-500 mt-3">{{ $t("trip_completed") }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
         </div>
-        <div class="grid grid-cols-12 gap-6 mt-5">
-          <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
-            <div class="report-box zoom-in">
-              <div class="box p-5">
-                <div class="flex align-middle">
-                  <div class="text-3xl font-medium leading-8 text-gray-500">-</div>
-                  <div class="ml-auto">
-                    <NavigationIcon class="report-box__icon text-gray-500" />
-                  </div>
-                </div>
-                <div class="text-base text-slate-500 mt-3">{{ $t("trip_created") }}</div>
-              </div>
-            </div>
-          </div>
-          <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
-            <div class="report-box zoom-in">
-              <div class="box p-5">
-                <div class="flex align-middle">
-                  <div class="text-3xl font-medium leading-8 text-orange-300">-</div>
-                  <div class="ml-auto">
-                    <AlertCircleIcon class="report-box__icon text-orange-300" />
-                  </div>
-                </div>
-                <div class="text-base text-slate-500 mt-3">{{ $t("trip_pending") }}</div>
-              </div>
-            </div>
-          </div>
-          <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
-            <div class="report-box zoom-in">
-              <div class="box p-5">
-                <div class="flex align-middle">
-                  <div class="text-3xl font-medium leading-8 text-blue-300">-</div>
-                  <div class="ml-auto">
-                    <ActivityIcon class="report-box__icon text-blue-300" />
-                  </div>
-                </div>
-                <div class="text-base text-slate-500 mt-3">{{ $t("trip_progress") }}</div>
-              </div>
-            </div>
-          </div>
-          <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
-            <div class="report-box zoom-in">
-              <div class="box p-5">
-                <div class="flex align-middle">
-                  <div class="text-3xl font-medium leading-8 text-green-300">-</div>
-                  <div class="ml-auto">
-                    <CheckIcon class="report-box__icon text-green-300" />
-                  </div>
-                </div>
-                <div class="text-base text-slate-500 mt-3">{{ $t("trip_completed") }}</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>  
+        <!-- END: Page Layout -->
+
     </div>
 
-  
 
-  </div>
-  <!-- END: Page Layout -->
+
 
 
 </template>
@@ -134,7 +141,7 @@ onMounted(async () => {
 
 </script>
 
-<style  scoped>
+<style scoped>
 table {
     border: black 1px solid;
 }
