c97ea2fb1dabbce53c23ef15e4c001fc906e163c
Dorian Gonzalez <doriandarren1@gmail.com>
2024-04-22 22:17:09 +0200

    main

--- src/components/template/private/Main.vue
+++ src/components/template/private/Main.vue
@@ -95,13 +95,13 @@
                 <nav class="flex flex-1 flex-col">
                     <ul role="list" class="flex flex-1 flex-col gap-y-7">
                         <li>
-                            <ul role="list" class="-mx-2 space-y-1">
-                              <li v-for="item in navigation" :key="item.name">
-                                  <router-link :to="{ name: item.href }" :class="[item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                                      <component :is="item.icon" class="h-6 w-6 shrink-0" aria-hidden="true" />
-                                      {{ item.name }}
-                                  </router-link>
-                              </li>
+                          <ul role="list" class="-mx-2 space-y-1">
+                            <li v-for="item in navigation" :key="item.name">
+                              <router-link :to="{ name: item.href }" :class="[item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                                  <component :is="item.icon" class="h-6 w-6 shrink-0" aria-hidden="true" />
+                                  {{ item.name }}
+                              </router-link>
+                            </li>
                           </ul>
                         </li>
                         <li>
