d7130d11e8a02d9c164dd19bcd45b03056089d54
Dorian Gonzalez <doriandarren1@gmail.com>
2024-04-25 23:03:52 +0200

    add main menu

--- src/components/template/private/Main.vue
+++ src/components/template/private/Main.vue
@@ -41,31 +41,6 @@
                                         alt="Your Company" />
                                 </div>
 
-                                <!-- <nav class="flex flex-1 flex-col">
-                                    <ul role="list" class="flex flex-1 flex-col gap-y-7">
-                                        <li>
-                                            <ul role="list" class="-mx-2 space-y-1">
-                                                <li v-for="item in navigation" :key="item.name">
-                                                    <a :href="item.href"
-                :class="[item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                                                        <component :is="item.icon" class="h-6 w-6 shrink-0"
-                                                            aria-hidden="true" />
-                                                        {{ item.name }}
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li class="mt-auto">
-                                            <a href="#"
-                                                class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
-                                                <Cog6ToothIcon class="h-6 w-6 shrink-0" aria-hidden="true" />
-                                                Settings
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </nav> -->
-
-
                                 <nav class="flex flex-1 flex-col">
                                     <ul role="list" class="flex flex-1 flex-col gap-y-7">
                                     <li>
@@ -117,31 +92,6 @@
                     <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                         alt="Your Company" />
                 </div>
-
-
-                <!-- <nav class="flex flex-1 flex-col">
-                    <ul role="list" class="flex flex-1 flex-col gap-y-7">
-                        <li>
-                          <ul role="list" class="-mx-2 space-y-1">
-                            <li v-for="item in navigation" :key="item.name">
-                              <router-link :to="{ name: item.href }" :class="[item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                                  <component :is="item.icon" class="h-6 w-6 shrink-0" aria-hidden="true" />
-                                  {{ item.name }}
-                              </router-link>
-                            </li>
-                          </ul>
-                        </li>
-                        <li class="mt-auto">
-                            <a href="#"
-                                class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
-                                <Cog6ToothIcon class="h-6 w-6 shrink-0" aria-hidden="true" />
-                                Settings
-                            </a>
-                        </li>
-                    </ul>
-                </nav> -->
-
-
                
                 <nav class="flex flex-1 flex-col">
                     <ul role="list" class="flex flex-1 flex-col gap-y-7">
