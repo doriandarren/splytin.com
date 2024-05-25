1346948d65d0e96660d03b3753b2f41513929520
Dorian Gonzalez <doriandarren1@gmail.com>
2024-05-25 22:18:52 +0200

    add

--- src/views/websocket/Websocket.vue
+++ src/views/websocket/Websocket.vue
@@ -77,7 +77,8 @@
         <input 
           v-model="newMessage" 
           placeholder="Enter message"
-          class="w-full rounded-full border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6" 
+          class="w-full rounded-full border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+          @keydown.enter="sendMessage"
         >
       </div>
 
