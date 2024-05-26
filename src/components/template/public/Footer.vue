27c0e2351e19e279a1dc6c8d87617bb7541210cd
Dorian Gonzalez <doriandarren1@gmail.com>
2024-05-26 16:24:58 +0200

    fix footer

--- src/components/template/public/Footer.vue
+++ src/components/template/public/Footer.vue
@@ -24,7 +24,7 @@
   </div>
   <div class="bg-gray-100">
     <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-      <p class="text-gray-500 text-sm text-center sm:text-left">© {{ year }} Splytin
+      <p class="text-gray-500 text-sm text-center sm:text-left mt-0">© {{ year }} Splytin
       </p>
       <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
         <a class="text-gray-500">
