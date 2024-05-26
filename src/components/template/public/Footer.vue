2d14d082d96040f547bea3d611859511a86c4229
Dorian Gonzalez <doriandarren1@gmail.com>
2024-05-26 16:46:28 +0200

    enhance login

--- src/components/template/public/Footer.vue
+++ src/components/template/public/Footer.vue
@@ -8,6 +8,7 @@
       </a>
       <p class="mt-2 text-sm text-gray-500"></p>
     </div>
+    
     <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
       <div class="lg:w-1/1 md:w-1/2 w-full px-4">
         <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Interés General</h2>
@@ -21,10 +22,24 @@
         </nav>
       </div>
     </div>
+
+    <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+      <div class="lg:w-1/1 md:w-1/2 w-full px-4">
+        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">{{ $t("resources") }}</h2>
+        <nav class="list-none mb-10">
+          <li>
+            <a href="https://splytin.com/privacy-policies" class="text-gray-600 hover:text-gray-800">{{ $t("privacity_polices") }}</a>
+          </li>
+          <li>
+            <a href="https://splytin.com/coockies" class="text-gray-600 hover:text-gray-800">{{ $t("coockies") }}</a>
+          </li>
+        </nav>
+      </div>
+    </div>
   </div>
   <div class="bg-gray-100">
     <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-      <p class="text-gray-500 text-sm text-center sm:text-left mt-0">© {{ year }} Splytin
+      <p class="text-gray-500 text-sm text-center sm:text-left mt-0">© {{ year }} {{ $t("site") }}
       </p>
       <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
         <a class="text-gray-500">
--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -1,12 +1,4 @@
 <template>
-    <!--
-      This example requires updating your template:
-  
-      ```
-      <html class="h-full bg-white">
-      <body class="h-full">
-      ```
-    -->
     <div class="flex min-h-full flex-1 mt-10">
       <div class="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
         <div class="mx-auto w-full max-w-sm lg:w-96">
@@ -23,33 +15,62 @@
           <div class="mt-10">
             <div>
               <form action="#" method="POST" class="space-y-6">
+
+                <!-- Email -->
                 <div>
                   <label for="email_input" class="block text-sm font-medium leading-6 text-gray-900">{{ $t("login_form.email") }}</label>
                   <div class="mt-2">
-                    <input type="text" id="email_input" v-model="email_input" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <input 
+                      type="text" 
+                      id="email_input" 
+                      v-model="email_input" 
+                      autocomplete="email" 
+                      required="" 
+                      class="block w-full px-3 rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                    />
                   </div>
                 </div>
-  
+
+                <!-- Password -->
                 <div>
                   <label for="password" class="block text-sm font-medium leading-6 text-gray-900">{{ $t("login_form.password") }}</label>
-                  <div class="mt-2">
-                    <input :type="showPassword ? 'text' : 'password'" id="password" v-model="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  <div class="relative">
+                    <input class="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
+                        :type="showPassword ? 'text' : 'password'" id="password" v-model="password" />
+                      <div class="absolute inset-y-0 right-0 flex items-center px-2">
+                          <button type="button" @click="togglePasswordVisibility">
+                              <img 
+                                  class="w-6 h-6"
+                                  :src="showPassword ? '/eye_off.svg' : '/eye_on.svg'"
+                                  alt="Show/Hide password" 
+                              />
+                          </button>
+                      </div>
                   </div>
                 </div>
+
+                
+
   
                 <div class="flex items-center justify-between">
                   <div class="flex items-center">
                     <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" />
-                    <label for="remember-me" class="ml-3 block text-sm leading-6 text-gray-700">Recuerdame</label>
+                    <label for="remember-me" class="ml-3 block text-sm leading-6 text-gray-700">{{ $t("login_form.remember_me") }}</label>
                   </div>
   
                   <div class="text-sm leading-6">
-                    <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Olvidastela contraseña?</a>
+                    <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">{{ $t("login_form.forgot_password") }}</a>
                   </div>
                 </div>
   
                 <div>
-                  <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" @click.prevent="submit">{{ $t("login_form.sign_in") }}</button>
+                  <button 
+                    @click.prevent="submit"
+                    type="submit" 
+                    class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" 
+                  >
+                    {{ $t("login_form.sign_in") }}
+                  </button>
                 </div>
               </form>
             </div>
@@ -205,32 +226,6 @@ const submit = async () => {
 
 
 
-
-
-
-    // fetch(`${import.meta.env.VITE_API_URL_GLOBALFLEET}auth/login`, {
-    //     method: "POST",
-    //     body: JSON.stringify(data),
-    //     headers: {
-    //         "Content-Type": "application/json"
-    //     },
-    // })
-    //     .then((res) => res.json())
-    //     .then((response) => {
-
-    //         console.log(response.token);
-
-
-    //         if (response.success && response.token) {
-    //             // message.value = 'datos correctos';
-    //             loading.value = false;
-    //             localStorage.setItem('splytin_token', response.token);
-    //             router.push('/dashboard');
-
-    //         } else {
-    //             message.value = 'datos incorrectos';
-    //         }
-    //     });
 }
 
 </script>
