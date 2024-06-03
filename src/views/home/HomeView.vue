a465d410a56f2b831c4eed267fdf9a548ae16fac
Dorian Gonzalez <doriandarren1@gmail.com>
2024-06-03 11:54:50 +0200

    enhance page home

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,81 +1,77 @@
 <template>
   <div v-animate>
 
-    <div class="container mx-auto mt-20">
-      <h1 class="sm:text-2xl text-2xl font-medium title-font mb-2 text-gray-900 text-center">
-        ¡Bienvenido a Splytin! Descubre la excelencia en entretenimiento con nuestros productos de alta calidad. 
-        <br>Explora y encuentra la solución perfecta para ti.
-      </h1>
+    <h1 class="sm:text-2xl text-2xl font-medium title-font text-gray-900 text-center mb-10">
+      ¡Bienvenido a Splytin! Descubre la excelencia en entretenimiento con nuestros productos de alta calidad.
+      <br>Explora y encuentra la solución perfecta para ti.
+    </h1>
 
 
-      <!-- <button class="btn-primary">
-        Save changes
-      </button> -->
-      
+    <div class="container mx-auto">
+      <div class="mx-auto">
+        <div class="flex flex-wrap w-full bg-gray-100 py-2 px-10 relative mb-2">
+          <div class="flex flex-wrap w-full flex-col items-center text-center mb-10">
+            <h1 class="sm:text-2xl text-2xl font-medium title-font mb-2 text-gray-900">
+              Nuestros Videos
+            </h1>
+          </div>
+          <iframe src="https://www.youtube.com/embed/VVYnVptxKKM" frameborder="0" allowfullscreen width="100%"
+            height="600vh"></iframe>
 
-    </div>
-
-
-
-   
-  <div class="container mx-auto ">
-    <div class="mx-auto">
-
-
-      <div class="flex flex-wrap w-full bg-gray-100 py-32 px-10 relative mb-4">
-        <div class="flex flex-wrap w-full flex-col items-center text-center mb-10">
-          <h1 class="sm:text-2xl text-2xl font-medium title-font mb-2 text-gray-900">
-            Nuestros Videos
-          </h1>
-        </div>
-        <iframe src="https://www.youtube.com/embed/VVYnVptxKKM" frameborder="0" allowfullscreen width="100%" height="600vh"></iframe>
-
-        <div class="text-center relative z-10 w-full">
-          <h2 class="text-2xl text-gray-900 font-medium title-font mb-2">
-            Nature Ambience Minecraft
-          </h2>
-          <a href="https://www.youtube.com/@FamiliaMusicMDDD" class="mt-3 text-indigo-500 inline-flex items-center">Más videos
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
-        </div>
-      </div>
-      <div class="flex flex-wrap -mx-2">
-        <div class="px-2 w-1/2">
-          <div class="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
-            <iframe src="https://www.youtube.com/embed/je-NfsNOP3g" frameborder="0" allowfullscreen width="100%" height="300vh"></iframe>
-            <div class="text-center relative z-10 w-full">
-              <h2 class="text-xl text-gray-900 font-medium title-font mb-2">
-                Relaxing Melodies
-              </h2>
-              <a href="https://www.youtube.com/@FamiliaMusicMDDD" class="mt-3 text-indigo-500 inline-flex items-center">Más videos
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
+          <div class="text-center relative z-10 w-full">
+            <h2 class="text-2xl text-gray-900 font-medium title-font mb-2">
+              Nature Ambience Minecraft
+            </h2>
+            <a href="https://www.youtube.com/@FamiliaMusicMDDD"
+              class="mt-3 text-indigo-500 inline-flex items-center">Más videos
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </a>
           </div>
         </div>
-        <div class="px-2 w-1/2">
-          <div class="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
-            <iframe src="https://www.youtube.com/embed/-o209ByIFcM" frameborder="0" allowfullscreen width="100%" height="300vh"></iframe>
-            <div class="text-center relative z-10 w-full">
-              <h2 class="text-xl text-gray-900 font-medium title-font mb-2">
-                Música que te ayuda a relajar, estudiar, dormir con Night Ambience
-              </h2>
-              <a href="https://www.youtube.com/@FamiliaMusicMDDD" class="mt-3 text-indigo-500 inline-flex items-center">Más videos
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
+        <div class="flex flex-wrap -mx-2">
+          <div class="px-2 w-1/2">
+            <div class="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
+              <iframe src="https://www.youtube.com/embed/je-NfsNOP3g" frameborder="0" allowfullscreen width="100%"
+                height="300vh"></iframe>
+              <div class="text-center relative z-10 w-full">
+                <h2 class="text-xl text-gray-900 font-medium title-font mb-2">
+                  Relaxing Melodies
+                </h2>
+                <a href="https://www.youtube.com/@FamiliaMusicMDDD"
+                  class="mt-3 text-indigo-500 inline-flex items-center">Más videos
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="px-2 w-1/2">
+            <div class="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
+              <iframe src="https://www.youtube.com/embed/-o209ByIFcM" frameborder="0" allowfullscreen width="100%"
+                height="300vh"></iframe>
+              <div class="text-center relative z-10 w-full">
+                <h2 class="text-xl text-gray-900 font-medium title-font mb-2">
+                  Música que te ayuda a relajar, estudiar, dormir con Night Ambience
+                </h2>
+                <a href="https://www.youtube.com/@FamiliaMusicMDDD"
+                  class="mt-3 text-indigo-500 inline-flex items-center">Más videos
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  
+
 
 
 
@@ -86,7 +82,7 @@
             Videos a la carta
           </h1>
         </div>
-        
+
         <div class="grid grid-cols-2 gap-10">
 
           <div class="col-span-2 md:col-span-2 lg:col-span-1">
@@ -108,7 +104,7 @@
             <iframe :src="videoUrl4" frameborder="0" allowfullscreen width="100%" height="300vh"></iframe>
           </div>
 
-          
+
 
         </div>
 
@@ -127,30 +123,31 @@
           </p>
         </div>
         <div class="flex flex-wrap -m-4">
-          
-            <div class="xl:w-1/3 md:w-1/2 p-4">
-              <a href="https://music.famindex.com/" target="_blank">
-            <div class="border border-gray-200 p-6 rounded-lg hover:bg-blue-100 hover:border-blue-300 hover:rounded-lg hover:shadow-xl ">
+
+          <div class="xl:w-1/3 md:w-1/2 p-4">
+            <a href="https://music.famindex.com/" target="_blank">
               <div
-                class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <g fill="none" fill-rule="evenodd">
-                    <path
-                      d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
-                    <path fill="currentColor"
-                      d="M18.671 3.208A2 2 0 0 1 21 5.18V17a4 4 0 1 1-2-3.465V9.18L9 10.847V18c0 .06-.005.117-.015.174A3.5 3.5 0 1 1 7 15.337v-8.49a2 2 0 0 1 1.671-1.973zM9 8.82l10-1.667V5.18L9 6.847z" />
-                  </g>
-                </svg>
+                class="border border-gray-200 p-6 rounded-lg hover:bg-blue-100 hover:border-blue-300 hover:rounded-lg hover:shadow-xl ">
+                <div
+                  class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <g fill="none" fill-rule="evenodd">
+                      <path
+                        d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                      <path fill="currentColor"
+                        d="M18.671 3.208A2 2 0 0 1 21 5.18V17a4 4 0 1 1-2-3.465V9.18L9 10.847V18c0 .06-.005.117-.015.174A3.5 3.5 0 1 1 7 15.337v-8.49a2 2 0 0 1 1.671-1.973zM9 8.82l10-1.667V5.18L9 6.847z" />
+                    </g>
+                  </svg>
+                </div>
+                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                  Famimusic
+                </h2>
+                <p class="leading-relaxed text-base">Página de Música y entretenimiento</p>
               </div>
-              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
-                Famimusic
-              </h2>
-              <p class="leading-relaxed text-base">Página de Música y entretenimiento</p>
-            </div>
-          </a>
+            </a>
           </div>
-          
-          
+
+
         </div>
       </div>
     </section>
