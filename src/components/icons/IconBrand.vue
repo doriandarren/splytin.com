a78a9d5f421d2e76d171b3d2170e07ffcf7244d9
Dorian Gonzalez <doriandarren1@gmail.com>
2024-08-02 16:32:34 +0200

    add icons

---
+++ src/components/icons/IconBrand.vue
@@ -0,0 +1,60 @@
+<template>
+    <!-- Generator: Adobe Illustrator 27.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        viewBox="0 0 1024 1024" style="enable-background:new 0 0 1024 1024;" xml:space="preserve">
+    <!-- <style type="text/css">
+        .st0{opacity:0.7;}
+        .st1{fill:#FFFFFF;}
+        .st2{opacity:0.8;}
+        .st3{opacity:0.72;}
+        .st4{opacity:0.77;}
+        .st5{opacity:0.9;}
+        .st6{opacity:0.4;}
+        .st7{opacity:0.56;}
+    </style> -->
+    <g>
+        <g>
+            <g class="st0">
+                <polygon class="st1" points="955.7329,0.0008 819.1995,34.1339 887.466,170.6672 			"/>
+            </g>
+            <g class="st2">
+                <polygon class="st1" points="101.6444,442.8317 136.534,580.2665 238.9339,512 			"/>
+            </g>
+            <g class="st0">
+                <polygon class="st1" points="375.4669,102.4003 409.6003,204.8002 614.4001,307.2002 546.1332,102.4003 			"/>
+            </g>
+            <g>
+                <polygon class="st1" points="136.534,580.2665 0.0006,1023.9987 238.9339,512 			"/>
+            </g>
+            <g class="st3">
+                <polygon class="st1" points="238.9339,512 0.0006,1023.9987 34.1341,1023.9987 375.4669,750.9329 614.4001,409.6001 			"/>
+            </g>
+            <g class="st4">
+                <polygon class="st1" points="375.4669,750.9329 682.6666,750.9329 614.4001,409.6001 			"/>
+            </g>
+            <g>
+                <polygon class="st1" points="887.466,170.6672 682.6666,750.9329 785.0661,1023.9987 819.1995,1023.9987 989.8659,580.2665 			
+                    "/>
+            </g>
+            <g class="st5">
+                <polygon class="st1" points="614.4001,409.6001 682.6666,750.9329 887.466,170.6672 			"/>
+            </g>
+            <g class="st6">
+                <polygon class="st1" points="614.4001,307.2002 614.4001,409.6001 887.466,170.6672 			"/>
+            </g>
+            <g>
+                <path class="st1" d="M819.1995,34.1339H614.4001l-68.2669,68.2665l68.2669,204.7998L887.466,170.6672L819.1995,34.1339z
+                    M648.5331,136.5338c-18.8513,0-34.133-15.2821-34.133-34.1335s15.2817-34.133,34.133-34.133s34.1335,15.2817,34.1335,34.133
+                    S667.3845,136.5338,648.5331,136.5338z"/>
+            </g>
+            <g class="st7">
+                <polygon class="st1" points="246.1182,921.5992 341.3334,1023.9987 375.4669,1023.9987 364.7987,819.1993 			"/>
+            </g>
+            <g class="st7">
+                <polygon class="st1" points="997.036,700.0521 921.5994,921.5988 989.8659,1023.9987 1023.9994,1023.9991 			"/>
+            </g>
+        </g>
+    </g>
+    </svg>
+
+</template>
