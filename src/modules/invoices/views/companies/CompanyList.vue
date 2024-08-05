eb1bafad94d95d1aeeff4e6d7f6c9e4fc59ecf09
mile <darimile@gmail.com>
2024-08-05 22:15:32 +0200

    fix changes

--- src/modules/invoices/views/companies/CompanyList.vue
+++ src/modules/invoices/views/companies/CompanyList.vue
@@ -1,5 +1,4 @@
 <template>
-
 	<!-- BEGIN: Page Layout Create -->
 	<div v-animate v-if="isCreate">
 		<Create
@@ -61,12 +60,12 @@
 					<button @click="showDeleteCompany(props.row.id)">
 						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-600 hover:text-red-400" viewBox="0 0 24 24"><path fill="currentColor" d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2zM7 6v13z"/></svg>
 					</button>
-					</span>
-				</template>
-			</VueGoodTable>
-		</div>
-
+				</span>
+			</template>
+		</VueGoodTable>
 	</div>
+	
+</div>
 
 </template>
 <script setup>
