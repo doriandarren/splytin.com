0639bc5641e8b18215e92ea700cb89b18f848fa5
Dorian Gonzalez <doriandarren1@gmail.com>
2024-08-17 20:08:00 +0200

    cahnge video form

--- src/modules/generators/components/generator_videos/GeneratorVideoCreate.vue
+++ src/modules/generators/components/generator_videos/GeneratorVideoCreate.vue
@@ -14,12 +14,12 @@
 							{{ $t("file") }} *
 						</label>
 						<input
-							v-model.trim="validate.file.$model"
 							id="file"
-							type="text"
+							type="file"
 							name="file"
 							class="form-control"
 							:class="{ 'border-danger': validate.file.$error }"
+							@change="handleFileUpload"
 						/>
 						<template v-if="validate.file.$error">
 							<div v-for="(error, index) in validate.file.$errors" :key="index" class="text-danger mt-2">
@@ -57,7 +57,7 @@
 
 <script setup>
 
-	import { onMounted, reactive, toRefs } from 'vue';
+	import { onMounted, reactive, render, toRefs } from 'vue';
 	import { required, minLength, maxLength, email, url, integer } from '@vuelidate/validators';
 	import { useVuelidate } from '@vuelidate/core';
 	import { helpers } from '@vuelidate/validators';
@@ -68,7 +68,7 @@
 
 	const rules = {
 		file: {
-			required: helpers.withMessage(t("form.required"), required),
+			//required: helpers.withMessage(t("form.required"), required),
 		},
 	};
 
@@ -78,14 +78,105 @@
 
 	const validate = useVuelidate(rules, toRefs(formData));
 
-	const save = () => {
+
+
+	const handleFileUpload = (event) => {
+		const selectedFile = event.target.files[0];
+  		formData.file = selectedFile; // Almacena el archivo directamente en formData.file
+
+  		//validate.value.$touch(); // Activar la validación si es necesario
+	};
+
+
+
+
+
+
+
+
+
+
+
+	// const save = async () => {
+	// 	validate.value.$touch();
+
+	// 	if (validate.value.$invalid) {
+	// 		console.log('Form is invalid');
+	// 		return;
+	// 	}
+
+	// 	// Preparar el FormData para la subida
+	// 	const payload = new FormData();
+	// 	payload.append('file', formData.file);
+
+	// 	// Enviar el archivo al servidor
+	// 	try {
+	// 		const response = await axios.post(`${import.meta.env.VITE_API_URL}generator-videos/store`, payload, {
+	// 		headers: {
+	// 			'Content-Type': 'multipart/form-data',
+	// 		},
+	// 		onUploadProgress: (progressEvent) => {
+	// 			console.log(
+	// 			`Upload progress: ${(progressEvent.loaded / progressEvent.total) * 100}%`
+	// 			);
+	// 		},
+	// 		});
+	// 		console.log('Archivo subido con éxito:', response.data);
+	// 		emit('saveGeneratorVideoForm', response.data); // Emitir la respuesta del servidor
+	// 	} catch (error) {
+	// 		console.error('Error al subir el archivo:', error);
+	// 	}
+	// };
+
+
+
+
+	const save = async () => {
 		validate.value.$touch();
+
 		if (validate.value.$invalid) {
-			//TODO
-		} else {
-			emit('saveGeneratorVideoForm', formData);
+			console.log('Form is invalid');
+			return;
+		}
+
+		// Preparar el FormData para la subida
+		const payload = new FormData();
+		payload.append('file', formData.file);
+
+		try {
+			const response = await fetch(`${import.meta.env.VITE_API_URL}generator-videos/store`, {
+			method: 'POST',
+			body: payload,
+			headers: {
+				// No establezcas `Content-Type` manualmente, dejar que `fetch` lo maneje
+				"Authorization": `Bearer ${localStorage.getItem('splytin_token')}`
+			},
+			});
+
+			if (!response.ok) {
+				throw new Error('Error al subir el archivo');
+			}
+
+			const data = await response.json();
+			console.log('Archivo subido con éxito:', data);
+			//emit('saveGeneratorVideoForm', data); // Emitir la respuesta del servidor
+		} catch (error) {
+			console.error('Error al subir el archivo:', error);
 		}
 	};
+
+
+
+	// const save = () => {
+
+	// 	validate.value.$touch();
+	// 	if (validate.value.$invalid) {
+	// 		//TODO
+	// 	} else {
+
+	// 		emit('saveGeneratorVideoForm', formData);
+	// 	}
+	// };
 
 	onMounted(async () => {
 		// TODO here implements...
