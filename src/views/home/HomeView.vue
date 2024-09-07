d924d0d23f35c57335a75c48148730b8be64489f
Dorian Gonzalez <doriandarren1@gmail.com>
2024-09-07 20:49:20 +0200

    fix modeules

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
 
-
   <!-- BEGIN Banner -->
   <div class="custom-div">
     <div class="animations">
@@ -218,58 +217,89 @@
           </div>
 
         </div>
-        <!-- Famimusic -->
-        <div class="xl:w-1/3 md:w-1/2 p-4 mb-5">
-          <a href="https://music.famindex.com/" target="_blank">
-            <div
-              class="border border-gray-200 bg-primary-light p-6 rounded-lg hover:bg-blue-100 hover:border-blue-300 hover:rounded-lg hover:shadow-xl">
-              <div
-                class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-700 mb-4">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <g fill="none" fill-rule="evenodd">
-                    <path
-                      d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
-                    <path fill="currentColor"
-                      d="M18.671 3.208A2 2 0 0 1 21 5.18V17a4 4 0 1 1-2-3.465V9.18L9 10.847V18c0 .06-.005.117-.015.174A3.5 3.5 0 1 1 7 15.337v-8.49a2 2 0 0 1 1.671-1.973zM9 8.82l10-1.667V5.18L9 6.847z" />
-                  </g>
-                </svg>
-              </div>
-              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
-                Famimusic
-              </h2>
-              <p class="leading-relaxed text-base">Página de Música y entretenimiento</p>
-            </div>
-          </a>
-        </div>
-        <!-- Famindex -->
-        <div class="xl:w-1/3 md:w-1/2 p-4 mb-5">
-          <a href="https://famindex.com/" target="_blank">
-            <div
-              class="border border-gray-200 bg-primary-light p-6 rounded-lg hover:bg-blue-100 hover:border-blue-300 hover:rounded-lg hover:shadow-xl">
-              <div
-                class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-700 mb-4">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                  <rect x="3" y="4" width="18" height="16" rx="2" ry="2"></rect>
-                  <path d="M3 8h18"></path>
-                  <path d="M7 12h5"></path>
-                  <path d="M7 16h5"></path>
-                  <path d="M16 12h3"></path>
-                  <path d="M16 16h3"></path>
-                </svg>
-              </div>
-              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
-                Famindex
-              </h2>
-              <p class="leading-relaxed text-base">Podrás conseguir las mejores templates</p>
-            </div>
-          </a>
-        </div>
+
+
+
+
 
 
       </div>
     </div>
   </section>
+
+
+  <section class="animate">
+
+    <div class="container mx-auto mt-10 mb-10">
+      <div class="flex">
+        <!-- Famimusic -->
+
+        <div class="md:w-1/2 xl:w-1/2">
+          <a href="https://music.famindex.com/" target="_blank">
+            <div
+              class="border border-blue-100 bg-blue-100 p-6 rounded-lg hover:bg-primary-light hover:rounded-lg hover:shadow-xl">
+              <div class="flex justify-center mb-4">
+                <div
+                  class="inline-flex items-center justify-center rounded-md bg-indigo-100 text-indigo-700 mr-3"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" viewBox="0 0 24 24">
+                    <g fill="none" fill-rule="evenodd">
+                      <path
+                        d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                      <path fill="currentColor"
+                        d="M18.671 3.208A2 2 0 0 1 21 5.18V17a4 4 0 1 1-2-3.465V9.18L9 10.847V18c0 .06-.005.117-.015.174A3.5 3.5 0 1 1 7 15.337v-8.49a2 2 0 0 1 1.671-1.973zM9 8.82l10-1.667V5.18L9 6.847z" />
+                    </g>
+                  </svg>
+                </div>
+
+                <h2 class="text-gray-900 font-medium title-font mt-1">
+                  Famimusic
+                </h2>
+              </div>
+              <p class="leading-relaxed text-base text-center">Página de Música y entretenimiento</p>
+            </div>
+          </a>
+        </div>
+
+        <!-- Famindex -->
+        <div class="md:w-1/2 xl:w-1/2 ml-10">
+          <a href="https://famindex.com/" target="_blank">
+            <div
+              class="border border-blue-100 bg-blue-100 p-6 rounded-lg hover:bg-primary-light hover:rounded-lg hover:shadow-xl">
+              <div class="flex justify-center mb-4">
+                <div
+                  class="inline-flex items-center justify-center rounded-md bg-indigo-100 text-indigo-700 mr-3"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <rect x="3" y="4" width="18" height="16" rx="2" ry="2"></rect>
+                    <path d="M3 8h18"></path>
+                    <path d="M7 12h5"></path>
+                    <path d="M7 16h5"></path>
+                    <path d="M16 12h3"></path>
+                    <path d="M16 16h3"></path>
+                  </svg>
+                </div>
+
+                <h2 class="text-gray-900 font-medium title-font mt-1">
+                  Famindex
+                </h2>
+              </div>
+              <p class="leading-relaxed text-base text-center">Podrás conseguir las mejores templates</p>
+            </div>
+          </a>
+        </div>
+
+      </div>
+
+
+    </div>
+
+
+
+  </section>
+
+
 
 </template>
 
