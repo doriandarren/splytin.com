d15345eb508791010a5735bd0b6af5b4fc796860
Dorian Gonzalez <doriandarren1@gmail.com>
2024-09-07 21:45:27 +0200

    fix footer

--- src/directives/animateDirective.js
+++ src/directives/animateDirective.js
@@ -5,4 +5,9 @@ export const animateDirective = {
     const animationClasses = binding.value || 'animate__animated animate__fadeInUp animate__fast';
     el.classList.add(...animationClasses.split(' '));
   },
+  beforeUpdate(el, binding) {
+    // Asegura que las clases de animación se apliquen también en las actualizaciones de componente
+    const animationClasses = binding.value || 'animate__animated animate__fadeInUp animate__fast';
+    el.classList.add(...animationClasses.split(' '));
+  },
 };
